770f5f795dae7a5b71876a48999a0f67b6994d6f
sinapxi <sinapxi@sinapxi.com>
2025-05-15 16:14:43 -0400

    Ajustar longpolling_port para que use la variable PORT de Railway

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Establecer el puerto por defecto si no está definido
 export PORT=${PORT:-8069}
-echo "Starting Odoo on port $PORT"
+echo "Starting Odoo on main port $PORT and longpolling on port $PORT"
 
 # Configurar variables de entorno adicionales si es necesario
 export PYTHONUNBUFFERED=1
@@ -13,6 +13,7 @@ cd /app
 exec python3 /app/odoo-bin \
     -c /app/odoo-arizon/odoo.conf \
     --http-port=$PORT \
+    --longpolling-port=$PORT \
     --db_host=${DB_HOST} \
     --db_port=${PGPORT} \
     --db_user=${PGUSER} \
